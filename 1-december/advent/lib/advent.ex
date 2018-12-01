51cbc3c2a41e38f646baeb9b3f4ddea83d20697f
Angelo Moreira <angelo.moreira@gmail.com>
2018-12-01 12:35:28 +0000

    second part of day 1, very inefficient algo

--- 1-december/advent/lib/advent.ex
+++ 1-december/advent/lib/advent.ex
@@ -12,18 +12,35 @@ defmodule Advent do
       8
 
   """
-  def counting(file) do
+  def counting(list_of_inst) do
+    list_of_inst
+    |> Enum.reduce(0, &(&1 + &2))
+  end
+
+  def file_to_ints(file) do
     case File.read(file) do
       {:ok, numbers} ->
         numbers
-        |> string_to_ints
-        |> Enum.reduce(0, &(&1 + &2))
+        |> String.split("\n")
+        |> Enum.map(&String.to_integer/1)
 
       {:error, reason} ->
         {:error, reason}
     end
   end
 
-  defp string_to_ints(list_of_strings),
-    do: list_of_strings |> String.split("\n") |> Enum.map(&String.to_integer/1)
+  def first_frequency(list_of_ints), do: find_first(list_of_ints, [0])
+
+  def find_first(numbers, acc) do
+    result =
+      Enum.reduce_while(numbers, acc, fn number, acc ->
+        frequency = List.last(acc) + number
+
+        if frequency in acc,
+          do: {:halt, frequency},
+          else: {:cont, acc ++ [frequency]}
+      end)
+
+    if is_integer(result), do: result, else: find_first(numbers, result)
+  end
 end

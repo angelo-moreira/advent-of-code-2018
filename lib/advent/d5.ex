9a30f6d1990f1574dd829683153ece4fed4b8ece
Angelo Moreira <angelo.m@designone.co.uk>
2018-12-05 22:47:54 +0000

    infinite loop but I think we are in the right track

--- lib/advent/d5.ex
+++ lib/advent/d5.ex
@@ -4,24 +4,48 @@ defmodule Advent.D5 do
     # |> File.read!()
     # |> String.split("", trim: true)
 
-    "dabAcCaCBAcCcaDA"
-    |> String.split("", trim: true)
-    |> Enum.reduce("", fn string, acc ->
-      first_string = String.first(acc)
+    clean_list("dabAcCaCBAcCcaDA", "", false)
+  end
 
-      is_capital? = string =~ ~r/^\p{Lu}$/u
+  def clean_list(string, acc, to_clean?) do
+    if string == "" do
+      {hd, tail} = get_head_and_tail(acc)
+      clean_list(tail, hd, to_clean?)
+    else
+      {hd, tail} = get_head_and_tail(string)
+      is_capital? = hd =~ ~r/^\p{Lu}$/u
+
+      {hd_acc, tail_acc} = get_head_and_tail(acc)
 
       cond do
-        acc == "" ->
-          string
+        hd == "" ->
+          if to_clean? do
+            x = clean_list("", acc, to_clean?)
+            require IEx
+            IEx.pry()
+            x
+          else
+            acc
+          end
 
-        is_capital? and String.downcase(string) == first_string ->
-          {_, tail} = acc |> String.to_charlist() |> List.pop_at(0)
-          List.to_string(tail)
+        is_capital? and String.downcase(hd) == hd_acc ->
+          clean_list(tail, tail_acc, true)
 
         true ->
-          string <> acc
+          clean_list(tail, hd <> acc, false)
       end
-    end)
+    end
+  end
+
+  defp get_head_and_tail(string) do
+    if string == "" do
+      {"", ""}
+    else
+      {hd, tail} = string |> String.to_charlist() |> List.pop_at(0)
+
+      hd = List.to_string([hd])
+      tail = List.to_string(tail)
+      {hd, tail}
+    end
   end
 end
